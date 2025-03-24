c42a177618d4c01f0892d08ef2b32046acb76c1e
FrogWithWheels <jakecallow42@gmail.com>
2025-03-24 12:38:34 +0000

    Changed debug settings to fix CSS on admin page.

--- WFDAssignment/settings.py
+++ WFDAssignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$pl_3lm0p0t#f-3oukw8*1dr((q!(!g_0&%16vgqsgghkpqx%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
