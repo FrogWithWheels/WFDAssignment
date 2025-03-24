6438c6698ff59474d520eea96e0ce40297570a96
FrogWithWheels <jakecallow42@gmail.com>
2025-03-24 12:26:26 +0000

    Added the initial blank pages and disabled the debug page.

--- WFDAssignment/settings.py
+++ WFDAssignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$pl_3lm0p0t#f-3oukw8*1dr((q!(!g_0&%16vgqsgghkpqx%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
