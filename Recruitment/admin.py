c049e6ec0603968a35f3f205126773d726a7f76a
FrogWithWheels <jakecallow42@gmail.com>
2025-04-13 12:00:31 +0100

    Added basic page and database API functionality. Basic template to display details of a candidate's account. Database model for candidate. Database model for trainer.

--- Recruitment/admin.py
+++ Recruitment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from Recruitment.models import Candidate
 
 # Register your models here.
+admin.site.register(Candidate)
--- Recruitment/views.py
+++ Recruitment/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Candidate
 
 def index(request):
     return HttpResponse("Recruitment page.")
+
+def account(request, candidate_id):
+    output = Candidate.objects.get(pk=1)
+    template = loader.get_template("recruitment/index.html")
+    context = {"user": output}
+    return HttpResponse(template.render(context, request))
